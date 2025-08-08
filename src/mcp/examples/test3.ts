31d8ab2bfee811a0e04bef1760019954f743f45c
dswbx <dennis.senn@gmx.ch>
2025-08-08 17:18:05 +0200

    reworked server, added stdio, initialized server sent notifications

--- src/mcp/examples/test3.ts
+++ src/mcp/examples/test3.ts
@@ -1,8 +1,16 @@
-// {
-//   content: [
-//     {
-//        type: "text",
-//        text: '{"ok":true,"message":"query: hello"}',
-//     },
-//   ],
-// }
+import { McpServer } from "../server";
+import { stdioTransport } from "../transports/stdio-transport";
+
+const server = new McpServer()
+   .tool("test", {}, async (params, c) => {
+      return c.text("Hello, world!");
+   })
+   .tool("test2", {}, async (params, c) => {
+      return c.text("Hello, world!");
+   });
+
+stdioTransport(server, {
+   stdin: process.stdin,
+   stdout: process.stdout,
+   stderr: process.stderr,
+});
